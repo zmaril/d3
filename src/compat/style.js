8bac7431df2e5a764f66a422555270b9a0010570
Zack Maril <zack@zacharymaril.com>
2012-05-17 20:09:45 -0200

    More info about the compat files and why they are needed

--- src/compat/style.js
+++ src/compat/style.js
@@ -1,3 +1,11 @@
+//A solution to fix the problem with IE9 and setting the style of an element.  
+// 
+// See [this commit](https://github.com/zmaril/d3/commit/c2e37352ac84eb8b4be89feed90bc2249866f715) for more information. 
+ 
+//Does the error happen? 
+// 
+//If so then change the prototype to include a function that does what setProperty is supposed to. 
+
 try {
   document.createElement("div").style.setProperty("opacity", 0, "");
 } catch (error) {
