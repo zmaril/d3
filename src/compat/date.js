7332a05fd4c7b34a685d18792a60f232af2bf1b0
Zack Maril <zack@zacharymaril.com>
2012-05-17 19:48:20 -0200

    Added in reasoning for why date.js exists.

--- src/compat/date.js
+++ src/compat/date.js
@@ -1,3 +1,5 @@
+//IE9 doesn't string coerce values. Errors were getting thrown left and right by this, so mbostock put in a shim for Date.now. 
+// See [this commit](https://github.com/zmaril/d3/commit/c2e37352ac84eb8b4be89feed90bc2249866f715) for more information. 
 if (!Date.now) Date.now = function() {
   return +new Date;
 };
