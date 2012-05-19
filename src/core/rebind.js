02ca76057c2ea5e366a5fafef511b69de1836264
Zack Maril <zack@zacharymaril.com>
2012-05-19 12:20:01 -0200

    Intro to rebind. Not very good.

--- src/core/rebind.js
+++ src/core/rebind.js
@@ -1,4 +1,21 @@
-// Copies a variable number of methods from source to target.
+// Copies a variable number of methods from source to target. See the [tests](https://github.com/mbostock/d3/blob/1b3a3b3cb3d234e550cfcb46bfd776883b5d458b/test/core/rebind-test.js) for further examples.
+//
+//             var a = {}, b = {foo: function() {that = this;}}, that
+//             d3.rebind(a,b,"foo")
+//             a.foo()
+//             that === b
+// 
+//             var a = {}, b = {foo: function() {that = this;}}, that
+//             d3.rebind(a,b,"foo")
+//             a.foo.call({})
+//             that === b
+// 
+//             var a = {}
+//             var b = {foo: function() {return 1;}, bro: function(){ return "Bro."}}
+//             d3.rebind(a,b,"foo","bar")
+//             a.foo() == 1
+//             b.bro() == "Bro."
+
 d3.rebind = function(target, source) {
   var i = 1, n = arguments.length, method;
   while (++i < n) target[method = arguments[i]] = d3_rebind(target, source, source[method]);
@@ -6,7 +23,9 @@ d3.rebind = function(target, source) {
 };
 
 // Method is assumed to be a standard D3 getter-setter:
+// 
 // If passed with no arguments, gets the value.
+// 
 // If passed with arguments, sets the value and returns the target.
 function d3_rebind(target, source, method) {
   return function() {
