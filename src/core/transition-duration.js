873f666971187eee244ea91db60492ec8a2644ec
Zack Maril <zack@zacharymaril.com>
2012-05-19 22:40:52 -0200

    Finding all these files I missed. Damn.

--- src/core/transition-duration.js
+++ src/core/transition-duration.js
@@ -4,3 +4,5 @@ d3_transitionPrototype.duration = function(value) {
       ? function(d, i, j) { groups[j][i].duration = Math.max(1, value.apply(this, arguments) | 0); }
       : (value = Math.max(1, value | 0), function(d, i, j) { groups[j][i].duration = value; }));
 };
+
+//Next: [core/transition-each.js](/d3/src/core/transition-each.html)
