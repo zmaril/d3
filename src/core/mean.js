d8bac2109b94da6311c18a0915b14320b6264c6a
Zack Maril <zack@zacharymaril.com>
2012-05-22 13:52:35 -0200

    Better mean.js

--- src/core/mean.js
+++ src/core/mean.js
@@ -1,4 +1,10 @@
-//Given an array, this function provides a [linear combination](http://en.wikipedia.org/wiki/Linear_combination) with the elements of the array acting as a [basis](http://en.wikipedia.org/wiki/Basis_(linear_algebra)).
+//Given an array, this function calculates the mean of the array. If you want, you can pass in a binary function that will be called on each element before the average is taken. 
+// 
+//              d3.mean([1,2,3])== 2
+//              d3.mean([1,2,3],function(d,i){return d+i;})== 3
+
+
+
 
 //Calls a function on each element as well. 
 d3.mean = function(array, f) {
@@ -15,5 +21,4 @@ d3.mean = function(array, f) {
   return j ? m : undefined;
 };
 
-//Did you know that a mean is just a linear 
 //Next: [core/median.js](/d3/src/core/median.html)
