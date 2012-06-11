405690b2da06a07d13445b33ed8d1bef114273d5
Zack Maril <zack@zacharymaril.com>
2012-06-11 12:53:53 -0200

    Basic intro to bisect

--- src/core/bisect.js
+++ src/core/bisect.js
@@ -1,3 +1,17 @@
+//A function which provides information about how to divide an array
+//into two parts. Let's look at a few simple examples first:
+//                 d3.bisect([1,2,3,4],1) == 1
+//                 d3.bisect([1,2,3,4],3) == 3
+//                 d3.bisect([1,2,3,4],4) == 5
+//                 d3.bisect([1,2,3,4],0) == 0
+//What's going on exactly? From some of the [orgininal documentation](https://github.com/mbostock/d3/blob/6ba3097766e39ab68cb3d04e4c8b81b6f51a29c5/src/core/bisect.js): 
+// ```Locate the insertion point for x in a to maintain sorted order. The
+// arguments lo and hi may be used to specify a subset of the array which should
+// be considered; by default the entire array is used. If x is already present
+// in a, the insertion point will be before (to the left of) any existing
+// entries. The return value is suitable for use as the first argument to
+// `array.splice` assuming that a is already sorted.```
+//TODO: FINISH explaining this
 d3.bisector = function(f) {
   return {
     left: function(a, x, lo, hi) {
