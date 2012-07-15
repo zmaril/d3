9ad7d15b243c70b7676bd308567851fd84d34d12
Zack Maril <zack@zacharymaril.com>
2012-07-15 14:07:39 -0200

    Commented the first fucntion

--- src/core/first.js
+++ src/core/first.js
@@ -1,3 +1,4 @@
+//This finds the lowest element of the array, via a given sort or the defeault d3.ascending. So `d3.first([1,2,3]) == 1` while `d3.first([1,2,3],d3.descending)== 3`. 
 d3.first = function(array, f) {
   var i = 0,
       n = array.length,
