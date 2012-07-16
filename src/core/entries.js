93a9caa7fdf1e26ff5546ef82617c7275d0e7d80
Zack Maril <zack@zacharymaril.com>
2012-07-15 22:05:06 -0200

    Commented entries function with simple example.

--- src/core/entries.js
+++ src/core/entries.js
@@ -1,3 +1,11 @@
+// Takes in an object and returns all of the key-value pairs from the object. 
+
+//A simple example: 
+//           
+// 
+//              var ob = {a:1, b:2, c:3}
+//              d3.entries(ob) == [{a:1}, {b:2}, {c:3}]
+
 d3.entries = function(map) {
   var entries = [];
   for (var key in map) entries.push({key: key, value: map[key]});
