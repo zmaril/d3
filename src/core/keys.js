a8a1c593319ccac83ff30265656f8fc80f4b5dd0
Zack Maril <zack@zacharymaril.com>
2012-07-15 22:02:16 -0200

    Keys and values with simple examples.

--- src/core/keys.js
+++ src/core/keys.js
@@ -1,3 +1,10 @@
+//Takes in a map and returns all of the keys from the map. 
+ 
+//A simple example :
+//           
+// 
+//              var ob = {a:1, b:2, c:3}
+//              d3.keys(ob) == ["a","b","c"]
 d3.keys = function(map) {
   var keys = [];
   for (var key in map) keys.push(key);
--- src/core/values.js
+++ src/core/values.js
@@ -1,3 +1,10 @@
+//Takes in a map and returns all of the values from the map. 
+ 
+//A simple example: 
+//           
+// 
+//              var ob = {a:1, b:2, c:3}
+//              d3.values(ob) == [1,2,3]
 d3.values = function(map) {
   var values = [];
   for (var key in map) values.push(map[key]);
