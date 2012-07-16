69673e62a1c4f0f5dab496da9edd634280654084
Zack Maril <zack@zacharymaril.com>
2012-07-15 22:14:59 -0200

    Commented the permute function. Gotta check if that example works as I think it does.

--- src/core/permute.js
+++ src/core/permute.js
@@ -1,3 +1,14 @@
+//Given an array of elements, and an array of indices, this function
+//takes the elements and rearranges them according to the order
+//specified by the indexes.
+
+//A simple example: 
+//           
+// 
+//              var array = ["Y","O","L","T"]
+//              var indexes =  [3,1,2,0]
+//              d3.permute(array,indexes).join("") == "TOLY"
+
 d3.permute = function(array, indexes) {
   var permutes = [],
       i = -1,
