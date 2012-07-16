ba8fcf7a8794561fc8adf7ed521ee64d013834cf
Zack Maril <zack@zacharymaril.com>
2012-07-15 23:57:21 -0200

    Did not know the round function did what it did.

--- src/core/round.js
+++ src/core/round.js
@@ -1,3 +1,11 @@
+//A better name for this function would have been d3.truncate. It chops off any decimal digits after the first `n` decimal digits. Useful for formatting. 
+
+//Examples:
+// 
+//          d3.round(Math.PI) == 3
+//          d3.round(Math.PI,1) == 3.1
+//          d3.round(Math.PI,5) == 3.14159
+
 d3.round = function(x, n) {
   return n
       ? Math.round(x * (n = Math.pow(10, n))) / n
