4ace4dc5964155a3fb6570d1f8d106c2c101b87b
Zack Maril <zack@zacharymaril.com>
2012-07-15 23:44:25 -0200

    Commenting the range function. They did the right thing with floating point math, but I am still wary of the whole thing.

--- src/core/range.js
+++ src/core/range.js
@@ -1,3 +1,17 @@
+//`d3.range` returns an array of numbers that represent an interval. 
+
+//Examples:
+//
+//         d3.range(5) // [0,1,2,3,4]
+//         d3.range(3,5) // [3,4]
+//         d3.range(300,500,50) // [300,350,400,450] 
+
+//Be careful with decimal step sizes. Never trust the precision of a
+//floating number to be exactly what you expect it to be. d3 does the
+//right thing and multiples and divides by magnitudes to calculate the
+//values from integers. Still, Floating point bugs can be subtle.
+// 
+//         d3.range(3,4,0.1) // [3,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9]
 d3.range = function(start, stop, step) {
   if (arguments.length < 3) {
     step = 1;
@@ -17,6 +31,7 @@ d3.range = function(start, stop, step) {
   return range;
 };
 
+//This calculates the order of magnitude needed to be able to manipulate the floating point values safely. So `f(1)->1, f(0.1)-> 10, f(0.2)->10,f(Math.PI-3)->100000000000000000`. 
 function d3_range_integerScale(x) {
   var k = 1;
   while (x * k % 1) k *= 10;
