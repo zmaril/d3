70998b8606b81dac9772c712d9550ac8043b62ad
Zack Maril <zack@zacharymaril.com>
2012-07-15 23:27:21 -0200

    Commenting collapse. Gets rid of spaces I guess?

--- src/core/collapse.js
+++ src/core/collapse.js
@@ -1,3 +1,4 @@
+//`d3_collapse` is an internal function that is used within the selection implementation when working with classes of elements. It is used to take out extra spaces and characters that might accumluate while working with the classes attached to elements as strings. 
 function d3_collapse(s) {
   return s.replace(/(^\s+)|(\s+$)/g, "").replace(/\s+/g, " ");
 }
